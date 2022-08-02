349dd6ba1edf2dc7970986ad77af52cc0dad9215
david-villamil <davillafer@outlook.es>
2022-08-02 18:22:41 +0200

    Design your taco

---
+++ taco-cloud/src/main/java/com/example/taco/cloud/domain/Taco.java
@@ -0,0 +1,13 @@
+package com.example.taco.cloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
