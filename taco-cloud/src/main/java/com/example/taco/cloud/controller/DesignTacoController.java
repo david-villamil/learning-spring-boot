47ba50273db38cdcb536c6478167121f347a8d61
david-villamil <davillafer@outlook.es>
2022-08-04 15:04:33 +0200

    Form validation and configurations

--- taco-cloud/src/main/java/com/example/taco/cloud/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/com/example/taco/cloud/controller/DesignTacoController.java
@@ -6,11 +6,10 @@ import com.example.taco.cloud.domain.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -47,6 +46,18 @@ public class DesignTacoController {
     public String showDesignForm(Model model) {
         model.addAttribute("taco", new Taco());
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco, Errors errors) {
+        // Validation
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        // TODO: Save the taco...
+        log.info("Processing taco: " + taco);
+
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
